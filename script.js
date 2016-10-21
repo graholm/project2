2cd67f732eb25e14f1960c442cb7edbacfa13a78
graholm <graholm@nuevaschool.org>
2016-10-21 09:26:43 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-//storeValue("name", "david")
-//storeValue("person", {name: "grace"})
+storeValue("name", "david")
+storeValue("person", {name: "grace"})
 
-//addListItem("userList", {name: "D"})
-//
-//////onNewListItem("userList", function(user){
-//// // console.log(user)
-////})
-//
-//onNewValue("person", function(newPerson) {
-//  console.log(newPerson)
-//})
+addListItem("userList", {name: "D"})   
+
+onNewListItem("userList", function(user){
+})
+
+onNewValue("person", function(newPerson) {
+
+})
 
 
-var input = $("#new-item-input")
+var name = $("#new-item-input")
 input.keydown(function(e) {	
   if (e.keyCode == "13") {
     var firstname = input.val();
