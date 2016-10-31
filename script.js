80299c15c27c20fef5c12bae7e312e7f006c3d6e
graholm <graholm@nuevaschool.org>
2016-10-31 09:26:12 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ onNewValue("person", function(newPerson) {
 var input = $("#new-item-input")
 
 enterButton.click(function() {
+  console.log ("firstname")
     var firstname = input.val();
     console.log (firstname)
     storeValue("userList", {name: firstname})
     $("body").append("Hello " +  firstname + "," + " what is your favorite animal?");
-   var element = document.createElement("input");
     
   })
 })
