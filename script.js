18025ff11a5c6e7ce3a7eb377873c06ebf25c4aa
graholm <graholm@nuevaschool.org>
2016-11-21 08:37:28 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ onNewValue("person", function(newPerson) {
 var input = $("#new-item-input")
 
 enterButton.click(function() {
-  console.log ("firstname")
     var firstname = input.val();
     console.log (firstname)
     storeValue("userList", {name: firstname})
